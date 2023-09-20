a941c7aad9e250480425a2e3e3d5f592a62d717e
Lamia114 <lamiaibri2011@gmail.com>
2023-09-20 15:48:59 +0400

    dd

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SelectComponent } from './select/select.component';
+import { QuestionComponent } from './question/question.component';
+import { ResultComponent } from './result/result.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  {path:'',component:SelectComponent},
+  {path:'question/:id',component:QuestionComponent},
+  {path:'result',component:ResultComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { QuestionComponent } from './question/question.component';
+import { SelectComponent } from './select/select.component';
+import { ResultComponent } from './result/result.component';
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuestionComponent,
+    SelectComponent,
+    ResultComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule, 
   ],
   providers: [],
   bootstrap: [AppComponent]
